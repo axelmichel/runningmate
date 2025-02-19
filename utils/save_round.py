eadd3e88dc7801786edffec3bf0a924e4517561a
Axel Michel <info@webandapp.fr>
2025-02-19 16:19:39 +0100

    [9]: Getting historical or current weather from open-meteo and pushing it into db

---
+++ utils/save_round.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+def safe_round(value, decimals=0):
+    """Safely rounds a number, avoiding NaN and None issues."""
+    if value is None or not isinstance(value, (int, float)) or pd.isna(value):
+        return 0  # Return 0 if value is None or NaN
+    return int(round(value, decimals))
